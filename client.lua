ba3e09eb3d70880c1ca81e26ce340d7ba75412d7
Proxo <lanefast1999@gmail.com>
2026-05-04 13:36:41 -0500

    Bump client loader cache after fixed bind revert

--- client.lua
+++ client.lua
@@ -1,1 +1,1 @@
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/ProxoMetatable/personal-lua-client/main/Loader.lua?v=11", true))()
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/ProxoMetatable/personal-lua-client/main/Loader.lua?v=12", true))()
