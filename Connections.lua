ff87055f430af7b3902dca516a2444990ec4f269
Proxo <lanefast1999@gmail.com>
2026-05-04 05:44:00 -0500

    Add connection manager module

---
+++ Connections.lua
@@ -0,0 +1,34 @@
+return function(context)
+    local Connections = {
+        items = {},
+    }
+
+    function Connections.add(name, connection)
+        Connections.disconnect(name)
+        Connections.items[name] = connection
+        return connection
+    end
+
+    function Connections.get(name)
+        return Connections.items[name]
+    end
+
+    function Connections.disconnect(name)
+        local connection = Connections.items[name]
+
+        if connection then
+            connection:Disconnect()
+            Connections.items[name] = nil
+        end
+    end
+
+    function Connections.disconnectAll()
+        for name in pairs(Connections.items) do
+            Connections.disconnect(name)
+        end
+    end
+
+    context.Connections = Connections
+
+    return Connections
+end
